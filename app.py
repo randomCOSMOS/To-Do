bb86d375f5ca1d56369948ae481198d1edb36bce
RANDOM cosmos <srijanwhat2@gmail.com>
2020-02-13 23:43:54 +0530

    better error handeling

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
@@ -12,9 +13,6 @@ class Todo(db.Model):
     content = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow())
 
-    def __repr__(self):
-        return '<Task %r>' % self.id
-
 
 @app.route('/')
 def hello_world():
@@ -22,6 +20,7 @@ def hello_world():
     return render_template('index.html', tasks=tasks)
 
 
+# entering task
 @app.route('/enterTask', methods=['POST'])
 def ok():
     task_content = request.form['content']
@@ -33,18 +32,14 @@ def ok():
             db.session.commit()
             return redirect('/')
         except:
-            return "There was an issue adding your request"
+            error = "There was an issue adding your request"
+            return redirect(url_for('got_error', err=error))
     else:
         error = "Input can't be blank"
         return redirect(url_for('got_error', err=error))
 
 
-@app.route('/enterTask/<err>')
-def got_error(err):
-    tasks = Todo.query.order_by(Todo.date_created).all()
-    return render_template('index.html', tasks=tasks, error=err)
-
-
+# deleting task
 @app.route('/delete/<int:id>')
 def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
@@ -54,7 +49,35 @@ def delete(id):
         db.session.commit()
         return redirect('/')
     except:
-        return 'There was a problem deleting that'
+        error = 'There was a problem deleting that'
+        return redirect(url_for('got_error', err=error))
+
+
+# updating task
+@app.route('/update/<int:id>', methods=['POST', 'GET'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task_content = request.form['content']
+
+        if task_content != "":
+            task.content = task_content
+            try:
+                db.session.commit()
+                return redirect('/')
+            except:
+                return redirect(url_for('got_error', err="There was a problem updating that."))
+        else:
+            return redirect(url_for('got_error', err="Oops! Can't be blank."))
+    else:
+        return render_template('update.html', task=task)
+
+
+# handling error
+@app.route('/error/<err>')
+def got_error(err):
+    tasks = Todo.query.order_by(Todo.date_created).all()
+    return render_template('index.html', tasks=tasks, error=err)
 
 
 if __name__ == '__main__':
